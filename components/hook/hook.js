0ab6bee4d3d0b0b704f450153b4824ab82f94a92
DerryCoffey <derry@siruss.co.uk>
2019-10-21 22:56:01 +0100

    Refactored the Hook into a component.

---
+++ components/hook/hook.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+import Code from "../code/code";
+import Clipboard from "../clipboard/clipboard";
+
+class Hook extends Component {
+  constructor(props){
+    super(props);
+    this.hook = props.hook;
+  }
+
+  render(){
+    return (
+      <section key={this.hook.name}>
+        <h2 className="title">
+          <a href={this.hook.link} target="_blank">{this.hook.name}</a>
+          <span>- {this.hook.author}</span>
+        </h2>
+        <p className="description">
+          {this.hook.description}
+        </p>
+        <main className="code-container">
+          <section className="code-section">
+            <h4 className="heading">Implementation</h4>
+            <Code>
+              {this.hook.implementationCode}
+            </Code>
+            <Clipboard />
+          </section>
+          <section className="code-section">
+            <h4 className="heading">Usage</h4>
+            <Code>
+              {this.hook.usageCode}
+            </Code>
+          </section>
+        </main>
+      </section>
+    )
+  }
+}
+
+export default Hook;
